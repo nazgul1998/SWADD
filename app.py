f971164f28db8718e12bd420c3ba5a778b34e6bf
Fernando A <fer.aliaga1998@gmail.com>
2022-06-08 21:16:41 -0500

    csrf agregado

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from routes.pacientes import pacientes
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os 
+from flask_wtf.csrf import CSRFProtect
 
+load_dotenv()
 app = Flask(__name__, template_folder="templates")
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+app.secret_key = os.getenv('SECRET_KEY').strip()
 
+print(app.secret_key)
 
-app.config.update(dict(
-    RECAPTCHA_ENABLED = True,
-    RECAPTCHA_SITE_KEY = "6Ld_MlYgAAAAALFIh30safrM4yyOw6JCVlnuS9oO" ,
-    RECAPTCHA_SECRET_KEY = "6Ld_MlYgAAAAAINLuF1Z5nNwrqCvpwDv7hUqwDjn" ,
-))
-app.config['SECRET_KEY'] = "N@zgul1998!!"
 #config db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost/SWADD'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
